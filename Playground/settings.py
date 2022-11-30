40f82b00241665062d43b55fde36ff3ce3768bc1
VictorCubaL <victorm.cubal17@gmail.com>
2022-11-29 23:13:32 -0500

    Mejorando el template

--- Playground/settings.py
+++ Playground/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Playground.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["D:/CarpetaGitHub/Playground/AppCoder/template"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
